b364b12c446382f98a6f9fc26cf6ad4e250e1bde
AleRodriguez <al22760049@ite.edu.mx>
2026-02-20 07:56:59 -0800

    fix seed.get

--- server/api/seed.get.ts
+++ server/api/seed.get.ts
@@ -1,19 +1,20 @@
+
 import { prisma } from '~/server/utils/prisma'
 import bcrypt from 'bcryptjs'
 
 export default defineEventHandler(async () => {
-  const count = await prisma.user.count()
-  if (count > 0) {
-    return { message: 'Ya existen usuarios' }
-  }
   const hash = await bcrypt.hash('Admin1234!', 10)
-  await prisma.user.create({
-    data: {
+  
+  await prisma.user.upsert({
+    where: { email: 'admin@empresa.com' },
+    update: { password: hash }, // Esto actualiza la contraseña si ya existe
+    create: {
       name: 'Administrador',
       email: 'admin@empresa.com',
       password: hash,
       role: 'ADMIN'
     }
   })
-  return { ok: true }
+  
+  return { ok: true, message: 'Usuario administrador actualizado con éxito' }
 })
